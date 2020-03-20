a47bad8816c871ea7f691ea40526135dba3f49ca
rongbin.xie <rongbin.xie@voyageone.cn>
2020-03-20 18:42:49 +0800

    修改vue router

--- src/_helpers/router.js
+++ src/_helpers/router.js
@@ -42,9 +42,9 @@ router.beforeEach((to, from, next) => {
     const authRequired = !publicPages.includes(to.path);
     const loggedIn = localStorage.getItem('user');
 
-    // if (authRequired && !loggedIn) {
-    //     return next('/login');
-    // }
+    if (authRequired && !loggedIn) {
+        return next('/login');
+    }
 
     next();
 });
